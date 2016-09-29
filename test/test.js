a6bde28ef600bcdbe624bf2c21b37f55452adc2c
Thomas Hobohm <superman3275@gmail.com>
2016-09-29 10:51:45 -0500

    Added responses stream test.

--- test/test.js
+++ test/test.js
@@ -23,8 +23,22 @@ test('test surveys', async t => {
   });
 });
 
-test('test questions', async t => {
+test('test details', async t => {
   const activity = new SurveyMonkey();
   const questionsStream = await activity.requestDetails(Object.assign(config, { survey_id: "85519823" }));
   t.equal(questionsStream.data.question_count, 1, "Assert survey with proper number of questions was retrieved.");
+});
+
+test('test responses', async t => {
+  const activity = new SurveyMonkey();
+  const details = await activity.requestResponses(Object.assign(config, { survey_id: "85519823" }));
+  details.on('data', (chunk) => {
+    t.comment(chunk);
+  });
+  details.on('error', (error) => {
+    t.fail(error);
+  });
+  details.on('end', () => {
+    t.comment('Completed streaming responses.');
+  });
 });
